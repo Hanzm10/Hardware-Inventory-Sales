fede9735aad949abdcecc4dc0d8cf9e262ec5fed
T480 <T480@DESKTOP-HIBPDRG>
2025-04-21 17:05:15 +0800

    add test workflow

---
+++ murico-swingapp/src/test/java/com/github/hanzm_10/murico/swingapp/lib/cache/TestLRU.java
@@ -0,0 +1,14 @@
+package com.github.hanzm_10.murico.swingapp.lib.cache;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestLRU {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
