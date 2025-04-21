a9dc57dae701df6a9420c6d3bcdcf0b7b62b7bbc
Ragudos <workemailaaronragudos@gmail.com>
2025-04-21 17:18:38 +0800

    add LRU cache tests

--- murico-swingapp/src/test/java/com/github/hanzm_10/murico/swingapp/lib/cache/TestLRU.java
+++ murico-swingapp/src/test/java/com/github/hanzm_10/murico/swingapp/lib/cache/TestLRU.java
@@ -2,13 +2,36 @@ package com.github.hanzm_10.murico.swingapp.lib.cache;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestLRU {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Test LRU Cache update()")
+	void testCacheUpdate() {
+		var cache = new LRU<String, String>(2);
+		
+		cache.update("foo", "bar");
+		
+		assertEquals(cache.get("foo"), "bar");
+		
+		cache.update("deez", "nuts");
+		cache.update("say", "what");
+		
+		assertEquals(cache.get("foo"), null);
+	}
+	
+	@Test
+	@DisplayName("Test LRU Cache get()")
+	void testCacheGet() {
+		var cache = new LRU<String, String>(2);
+		
+		assertEquals(cache.get("ha"), null);
+		
+		cache.update("ha", "hatdog");
+	
+		assertEquals(cache.get("ha"), "hatdog");
 	}
 
 }
