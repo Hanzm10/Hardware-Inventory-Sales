450f0ca4c64d4db19ab913f9114670ec99f34dc8
tip-aaron <qaamragudos@tip.edu.ph>
2025-05-05 01:12:28 +0800

    🗃️ refactor sql query for getting user by email
    Signed-off-by: tip-aaron <qaamragudos@tip.edu.ph>

--- murico-swingapp/src/main/resources/com/github/hanzm_10/murico/swingapp/lib/database/sql/mysql/select/users/get_user_by_email.sql
+++ murico-swingapp/src/main/resources/com/github/hanzm_10/murico/swingapp/lib/database/sql/mysql/select/users/get_user_by_email.sql
@@ -1,7 +1,6 @@
-SELECT
-  _user_id, _user_created_at, user_display_name, user_display_image, user_gender
-FROM
-  users
-  JOIN user_credentials USING (_user_id)
-WHERE
-  user_email = ?;
+SELECT * FROM users
+WHERE _user_id = (
+	SELECT _user_id
+	FROM accounts
+	WHERE email = $1
+);
