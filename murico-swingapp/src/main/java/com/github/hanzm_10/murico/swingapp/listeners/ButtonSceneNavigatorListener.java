f48cf9c824a5006560dfcdec4421516632f32d5d
tip-aaron <qaamragudos@tip.edu.ph>
2025-05-05 21:11:42 +0800

    ♻️ refactor ButtonSceneNavigatorListener to use AtomicBoolean
    Signed-off-by: tip-aaron <qaamragudos@tip.edu.ph>

--- murico-swingapp/src/main/java/com/github/hanzm_10/murico/swingapp/listeners/ButtonSceneNavigatorListener.java
+++ murico-swingapp/src/main/java/com/github/hanzm_10/murico/swingapp/listeners/ButtonSceneNavigatorListener.java
@@ -2,20 +2,21 @@ package com.github.hanzm_10.murico.swingapp.listeners;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
 import com.github.hanzm_10.murico.swingapp.lib.navigation.SceneNavigator;
 
 public class ButtonSceneNavigatorListener implements ActionListener {
-	private final Supplier<Boolean> disableNavigation;
+	private final Supplier<AtomicBoolean> disableNavigation;
 
-	public ButtonSceneNavigatorListener(Supplier<Boolean> disableNavigation) {
+	public ButtonSceneNavigatorListener(Supplier<AtomicBoolean> disableNavigation) {
 		this.disableNavigation = disableNavigation;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (disableNavigation.get()) {
+		if (disableNavigation.get().get()) {
 			return;
 		}
 
