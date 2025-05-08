8a43068d70affa585786d2563f153f36c1c18907
tip-aaron <qaamragudos@tip.edu.ph>
2025-05-09 00:21:02 +0800

    🔥 remove sql-queries folder
    Signed-off-by: tip-aaron <qaamragudos@tip.edu.ph>

--- sql-queries/V1__seed_database.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (display_name) VALUES ("Aaron", "Peter", "Hanz", "Jerick", "Kurt");
-
-INSERT INTO accounts (_user_id, email, password_hash, password_salt, verification_status) VALUES
-(1,),
-(2,),
-(3,)
-(4,),
-(5,);
