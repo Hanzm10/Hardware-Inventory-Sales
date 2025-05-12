79eebf4e93cc43f2b80c98f8bb2b9eac08a41c9b
tip-aaron <qaamragudos@tip.edu.ph>
2025-05-12 14:11:32 +0800

    fix merge conflict resolution in .classpath
    Signed-off-by: tip-aaron <qaamragudos@tip.edu.ph>

--- murico-swingapp/src/main/java/com/github/hanzm_10/murico/swingapp/scenes/ProfileScene.java
+++ murico-swingapp/src/main/java/com/github/hanzm_10/murico/swingapp/scenes/ProfileScene.java
@@ -1,41 +1,40 @@
 package com.github.hanzm_10.murico.swingapp.scenes;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-import java.util.logging.Level;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 
-import com.github.hanzm_10.murico.swingapp.assets.AssetManager;
 import com.github.hanzm_10.murico.swingapp.lib.navigation.scene.Scene;
-import com.github.hanzm_10.murico.swingapp.listeners.ButtonSceneNavigatorListener;
-import com.github.hanzm_10.murico.swingapp.listeners.TogglePasswordFieldVisibilityListener;
-import com.github.hanzm_10.murico.swingapp.ui.buttons.ButtonStyles;
-import com.github.hanzm_10.murico.swingapp.ui.buttons.StyledButtonFactory;
 import com.github.hanzm_10.murico.swingapp.ui.components.panels.ImagePanel;
-import com.github.hanzm_10.murico.swingapp.ui.components.panels.Line;
-import com.github.hanzm_10.murico.swingapp.ui.components.panels.RoundedImagePanel;
-import com.github.hanzm_10.murico.swingapp.ui.inputs.TextFieldFactory;
-import com.github.hanzm_10.murico.swingapp.ui.inputs.TextPlaceholder;
-import com.github.hanzm_10.murico.swingapp.ui.labels.LabelFactory;
 
 import net.miginfocom.swing.MigLayout;
 
 public class ProfileScene implements Scene {
-	
+
 	protected JPanel view;
 	protected JPanel leftComponent;
-	
+
 	protected JLabel profLabel;
 	protected Image logoImage;
 	protected ImagePanel logo;
 	protected JLabel usernameLabel;
+
+	private void attachComponents() {
+		leftComponent.add(usernameLabel, "cell 0 2 3, grow");
+	}
+
+	private void createProfComponents() {
+		profLabel = new JLabel("Profile");
+		profLabel.setFont(profLabel.getFont().deriveFont(Font.BOLD, 32));
+
+		usernameLabel = new JLabel("Name Placeholder");
+		usernameLabel.setFont(usernameLabel.getFont().deriveFont(Font.BOLD, 24));
+		usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+	}
 
 	@Override
 	public String getSceneName() {
@@ -49,30 +48,16 @@ public class ProfileScene implements Scene {
 
 	@Override
 	public void onCreate() {
-		view = new JPanel(new MigLayout("fill", "[grow]", "[grow]")); 
-		leftComponent = new JPanel(new MigLayout("", "[grow]", "[]20[]")); 
+		view = new JPanel(new MigLayout("fill", "[grow]", "[grow]"));
+		leftComponent = new JPanel(new MigLayout("", "[grow]", "[]20[]"));
 
 		createProfComponents();
-		attachComponents(); 
+		attachComponents();
 
-		view.add(leftComponent, "grow"); 
+		view.add(leftComponent, "grow");
 	}
-	
-	private void createProfComponents() {
-		profLabel = new JLabel("Profile");
-		profLabel.setFont(profLabel.getFont().deriveFont(Font.BOLD, 32));
 
-		usernameLabel = new JLabel("Name Placeholder");
-		usernameLabel.setFont(usernameLabel.getFont().deriveFont(Font.BOLD, 24));
-		usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		
-	}
-	
 	public void setUsername(String username) {
 		usernameLabel.setText(username);
-	}
-	
-	private void attachComponents() {
-		leftComponent.add(usernameLabel, "cell 0 2 3, grow");
 	}
 }
